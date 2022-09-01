c54224f8e6c00143d3a8784958073fab2ff2013e
David Sundgren <sundgren.david@gmail.com>
2022-09-01 16:11:27 +0200

    exercise lesson 2

---
+++ lesson_2/Lesson_2_input.py
@@ -0,0 +1,8 @@
+name = input("Hello, what's your name?")
+print(f'Your name is {name}')
+number = input("What's your lucky number?")
+print(f'Your lucky number is {int(number)}, the double is {int(number)*2}')
+h = input("Say hello")
+print(f'{h+h}')
+a = input("Give me a float number")
+print(f'{float(a)/3.5}')
---
+++ lesson_2/Lesson_2_printing.py
@@ -0,0 +1,18 @@
+from types import NoneType
+
+
+msg = "hello world"
+print(msg)
+x = 10
+print(x)
+y = 10.12
+print(y)
+z = 10>9
+print(z)
+a = 10<9
+print(a)
+b = True
+print(b)
+c = None
+print(c)
+print(f'{msg},{x},{y},{z},{a},{b},{c}')
