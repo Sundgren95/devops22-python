8eb1f8f9ab0598c9e4130e44b90009b6fdb7bcb1
David Sundgren <sundgren.david@gmail.com>
2022-09-04 20:24:08 +0200

    lesson 2 utökade

---
+++ lesson_2/Lesson_2_utökade_övningar.py
@@ -0,0 +1,37 @@
+from math import pi
+
+a = 5
+b = a
+a = 7
+c = a+b
+print(c)
+
+a = 7*3+1
+print(a)
+
+a = 20/4*2
+print(int(a))
+print(float(a))
+
+a = 1
+b = a
+a = a + b
+b = a + b
+c = a + b
+print(c)
+
+num1 = int(input("give me a number"))
+num2 = int(input("give me a number"))
+diff = abs(num1-num2)
+print(int(num1+num2))
+print(diff)
+print(int(num1*num2))
+
+num1 = int(input("give me a number"))
+num2 = int(input("give me a number"))
+print(int(num1/num2))
+a = 20/4*2
+print(a)
+print(int(a))
+
+r = int(input("give me the radius of the cirkle"))
